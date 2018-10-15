65ab5bbc4f248db0490fd57abe0db20750d3d29e
Roberto Vidal <vidal.roberto.j@gmail.com>
2018-10-15 08:16:56 +0200

    wasm32-unknown-unknown

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
 //! A (hopefully) R5RS compliant Scheme interpreter
-extern crate env_logger;
 #[macro_use]
 extern crate gc;
 #[macro_use]
 extern crate log;
+#[cfg(all(target_arch = "wasm32", target_os = "unknown"))]
+extern crate wasm_bindgen;
 
 #[macro_use]
 mod helpers;
@@ -14,3 +15,8 @@ mod interpreter;
 pub mod lexer;
 pub mod reader;
 pub mod vm;
+
+#[cfg(all(target_arch = "wasm32", target_os = "unknown"))]
+mod web;
+#[cfg(all(target_arch = "wasm32", target_os = "unknown"))]
+pub use web::*;
